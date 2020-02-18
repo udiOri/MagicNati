d3b3eab67f73e6955a8b78de44de7e074a974c22
Udi Ori <udiori@gmail.com>
2020-02-18 21:10:53 +0200

    Added all animation using GreenSock

--- src/app/cauldron/cauldron.component.ts
+++ src/app/cauldron/cauldron.component.ts
@@ -13,13 +13,16 @@ export class CauldronComponent implements OnInit {
   myData: any = [];
   expirementArray: expirementModel[] = [];
   arrayFull = false;
+
   exp1: string = "";
   exp2: string = "";
   exp3: string = "";
-  mixes: boolean = false; // fruit , honey , red herring
-  slices: boolean = false; // milk , salt , peppermint
-  grinds: boolean = false; // magic ink , crocodile tears
-  stirs: boolean = false; // dragon sweat , soot
+
+  mixes: boolean = false; // fruit , honey , red herring // side to side
+  slices: boolean = false; // milk , salt , peppermint // up and down
+  grinds: boolean = false; // magic ink , crocodile tears // down+grow back+ shrink
+  stirs: boolean = false; // dragon sweat , soot // side to side
+
   @ViewChild("fruit", { static: false }) fruit: ElementRef;
   @ViewChild("honey", { static: false }) honey: ElementRef;
   @ViewChild("herring", { static: false }) herring: ElementRef;
@@ -83,5 +86,180 @@ export class CauldronComponent implements OnInit {
         return;
       }
     }
+  }
+  ///////////////////////////////// process animations
+  animationMixes() {
+    this.mixes = true;
+    setTimeout(() => {
+      this.animationFruit();
+    }, 1000);
+    setTimeout(() => {
+      this.animationHoney();
+    }, 2000);
+    setTimeout(() => {
+      this.animationHerring();
+    }, 3000);
+  }
+
+  animationSlices() {
+    this.slices = true;
+    setTimeout(() => {
+      this.animationMilk();
+    }, 1000);
+    setTimeout(() => {
+      this.animationSalt();
+    }, 2000);
+    setTimeout(() => {
+      this.animationPepper();
+    }, 3000);
+  }
+
+  animationGrinds() {
+    this.grinds = true;
+    setTimeout(() => {
+      this.animationMagic();
+    }, 1000);
+    setTimeout(() => {
+      this.animationCrock();
+    }, 2000);
+  }
+
+  animationStirs() {
+    this.stirs = true;
+    setTimeout(() => {
+      this.animationSoot();
+    }, 1000);
+    setTimeout(() => {
+      this.animationDragon();
+    }, 2000);
+  }
+
+  ///////////////////////////////////////////////////////// ingr animations
+
+  animationFruit() {
+    let tl = new TimelineMax();
+    tl.to(this.fruit.nativeElement, 0.25, { y: 300, ease: Power1.easeInOut })
+      .to(this.fruit.nativeElement, 0.25, { x: 150 })
+      .to(this.fruit.nativeElement, 0.25, { x: -150 })
+      .to(this.fruit.nativeElement, 0.25, {
+        x: 60,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+
+  animationHoney() {
+    let tl = new TimelineMax();
+    tl.to(this.honey.nativeElement, 0.25, { y: 300, ease: Power1.easeInOut })
+      .to(this.honey.nativeElement, 0.25, { x: 150 })
+      .to(this.honey.nativeElement, 0.25, { x: -150 })
+      .to(this.honey.nativeElement, 0.25, {
+        x: 60,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+
+  animationHerring() {
+    let tl = new TimelineMax();
+    tl.to(this.herring.nativeElement, 0.25, { y: 300, ease: Power1.easeInOut })
+      .to(this.herring.nativeElement, 0.25, { x: 150 })
+      .to(this.herring.nativeElement, 0.25, { x: -150 })
+      .to(this.herring.nativeElement, 0.25, {
+        x: 60,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+
+  animationMilk() {
+    let tl = new TimelineMax();
+    tl.to(this.milk.nativeElement, 0.3, {
+      y: 300,
+      ease: Power1.easeInOut
+    })
+      .to(this.milk.nativeElement, 0.3, { y: -100 })
+      .to(this.milk.nativeElement, 0.3, {
+        y: 300,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+  animationSalt() {
+    let tl = new TimelineMax();
+    tl.to(this.salt.nativeElement, 0.3, {
+      y: 300,
+      ease: Power1.easeInOut
+    })
+      .to(this.salt.nativeElement, 0.3, { y: -100 })
+      .to(this.salt.nativeElement, 0.3, {
+        y: 300,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+  animationPepper() {
+    let tl = new TimelineMax();
+    tl.to(this.peppermint.nativeElement, 0.3, {
+      y: 300,
+      ease: Power1.easeInOut
+    })
+      .to(this.peppermint.nativeElement, 0.3, { y: -100 })
+      .to(this.peppermint.nativeElement, 0.3, {
+        y: 300,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+
+  animationMagic() {
+    let tl = new TimelineMax();
+    tl.to(this.magic.nativeElement, 0.3, {
+      y: 300,
+      ease: Power1.easeInOut
+    })
+      .to(this.magic.nativeElement, 0.3, { y: -100 })
+      .to(this.magic.nativeElement, 0.3, {
+        y: 300,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+  animationCrock() {
+    let tl = new TimelineMax();
+    tl.to(this.crocodile.nativeElement, 0.3, {
+      y: 300,
+      ease: Power1.easeInOut
+    })
+      .to(this.crocodile.nativeElement, 0.3, { y: -100 })
+      .to(this.crocodile.nativeElement, 0.3, {
+        y: 300,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+
+  animationSoot() {
+    let tl = new TimelineMax();
+    tl.to(this.soot.nativeElement, 0.25, { y: 300, ease: Power1.easeInOut })
+      .to(this.soot.nativeElement, 0.25, { x: 150 })
+      .to(this.soot.nativeElement, 0.25, { x: -150 })
+      .to(this.soot.nativeElement, 0.25, {
+        x: 60,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
+  }
+
+  animationDragon() {
+    let tl = new TimelineMax();
+    tl.to(this.dragon.nativeElement, 0.25, { y: 300, ease: Power1.easeInOut })
+      .to(this.dragon.nativeElement, 0.25, { x: 150 })
+      .to(this.dragon.nativeElement, 0.25, { x: -150 })
+      .to(this.dragon.nativeElement, 0.25, {
+        x: 60,
+        opacity: 0,
+        ease: Power1.easeInOut
+      });
   }
 }
